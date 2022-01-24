a8c47d996614cf1346f56471b61b1ef1830179b1
Dmitry Khilko <dmitry.jilko@gmail.com>
2022-01-24 10:27:10 +0100

    first commit

---
+++ node-course/app.js
@@ -0,0 +1,6 @@
+// const helpers = require("./helpers");
+const {sum,add} = require("./helpers");
+const http=require('http');
+
+const total=sum(150,20);
+console.log("Total: ",total);
---
+++ node-course/helpers.js
@@ -0,0 +1,8 @@
+// function sum(a,b) {
+//     return a+b;
+// }
+
+exports.sum=(a,b)=> a+b;
+exports.add=(a,b)=> a+b;
+
+// console.log("Prosess :" ,process);
